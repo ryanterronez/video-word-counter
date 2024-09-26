37ba08fba5d21644bdad32cc84dd632284c0c38d
ryan terronez <ryanterronez@gmail.com>
2024-09-26 09:57:54 -0500

    added eslint

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  env: {
-    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY
-  }
+  transpileDependencies: true
 })
