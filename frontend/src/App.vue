9842cbbebfd7fd9741a6e85b98fb01a82b0b387c
ryan terronez <ryanterronez@gmail.com>
2024-09-26 11:44:18 -0500

    created frontend and backend directories

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <SearchBar />
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar.vue'
+
+export default {
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style>
+/* Add any global styles here */
+</style>
