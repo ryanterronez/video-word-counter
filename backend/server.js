bd649f198881801b95ad423853cd83eef47e8a6b
ryan terronez <ryanterronez@gmail.com>
2024-09-30 18:00:37 -0500

    updated transcript to not run if env set could cost too much money

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,16 @@ const cors = require('cors');
 const YtDlpWrap = require('yt-dlp-wrap').default;
 const fs = require('fs');
 const path = require('path');
-const wav = require('wav');
 const ffmpeg = require('fluent-ffmpeg');
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { TranscribeClient, StartTranscriptionJobCommand, GetTranscriptionJobCommand } = require('@aws-sdk/client-transcribe');
 const axios = require('axios');
+require('dotenv').config()
 
 const app = express();
 const port = 3000;
+const s3Bucket = 'audio-transcription-node-20';
 
-// Configure AWS SDK v3 clients
 const s3Client = new S3Client({ region: 'us-east-2' });
 const transcribeClient = new TranscribeClient({ region: 'us-east-2' });
 
@@ -23,6 +23,21 @@ app.post('/extract_audio', async (req, res) => {
   const videoUrl = req.body.video_url;
   const videoTitle = req.body.video_title;
 
+  const audioFilePath = await extractAudio(videoUrl, videoTitle);
+  if (!fs.existsSync(audioFilePath)) {
+    throw new Error(`File not found: ${audioFilePath}`);
+  }
+
+  if (process.env.VUE_APP_LOCAL) {
+    console.log('Local mode, skipping transcription');
+    res.status(200).json({ message: 'Local mode, skipping transcription' });
+  } else {
+  const transcript = await transcribeAudio(audioFilePath);
+  res.status(200).json({ message: 'Audio extracted and transcribed successfully', transcript });
+  }
+});
+
+async function extractAudio(videoUrl, videoTitle) {
   const outputDir = path.join(__dirname, 'downloads');
   const outputFile = path.join(outputDir, `${videoTitle}.mp3`);
 
@@ -39,19 +54,14 @@ app.post('/extract_audio', async (req, res) => {
       '--audio-format', 'mp3',
       '-o', outputFile
     ]);
-
-    const audioFilePath = outputFile.replace('%(title)s', 'your_audio_file_name').replace('%(ext)s', 'mp3');
-    if (!fs.existsSync(audioFilePath)) {
-      throw new Error(`File not found: ${audioFilePath}`);
-    }
-
-    const transcript = await transcribeAudio(audioFilePath);
-    res.status(200).json({ message: 'Audio extracted and transcribed successfully', transcript });
   } catch (error) {
     console.error(`Error: ${error}`);
-    res.status(500).json({ error: error.message });
+    throw error;
   }
-});
+  const audioFilePath = outputFile.replace('%(title)s', 'your_audio_file_name').replace('%(ext)s', 'mp3');
+
+  return audioFilePath;
+}
 
 async function transcribeAudio(audioFilePath) {
   return new Promise((resolve, reject) => {
@@ -67,7 +77,7 @@ async function transcribeAudio(audioFilePath) {
         const params = {
           LanguageCode: 'en-US',
           Media: {
-            MediaFileUri: `s3://audio-transcription-node-20/${s3Key}`
+            MediaFileUri: `s3://${s3Bucket}/${s3Key}`
           },
           TranscriptionJobName: `TranscriptionJob-${Date.now()}`,
           MediaFormat: 'wav',
@@ -77,17 +87,16 @@ async function transcribeAudio(audioFilePath) {
           const data = await transcribeClient.send(new StartTranscriptionJobCommand(params));
           const jobName = data.TranscriptionJob.TranscriptionJobName;
         
-          // Poll for job completion
           const checkJobStatus = async () => {
             const jobData = await transcribeClient.send(new GetTranscriptionJobCommand({ TranscriptionJobName: jobName }));
             if (jobData.TranscriptionJob.TranscriptionJobStatus === 'COMPLETED') {
               const transcriptUri = jobData.TranscriptionJob.Transcript.TranscriptFileUri;
-              console.log(`Transcript URI: ${transcriptUri}`); // Debugging: Log the transcript URI
+              console.log(`Transcript URI: ${transcriptUri}`);
               try {
                 const transcriptData = await axios.get(transcriptUri);
                 resolve(transcriptData.data.results.transcripts[0].transcript);
               } catch (fetchError) {
-                console.error(`Error fetching transcript: ${fetchError.message}`); // Debugging: Log fetch error
+                console.error(`Error fetching transcript: ${fetchError.message}`);
                 reject(fetchError);
               }
             } else if (jobData.TranscriptionJob.TranscriptionJobStatus === 'FAILED') {
@@ -99,7 +108,7 @@ async function transcribeAudio(audioFilePath) {
         
           checkJobStatus();
         } catch (error) {
-          console.error(`Error starting transcription job: ${error.message}`); // Debugging: Log start job error
+          console.error(`Error starting transcription job: ${error.message}`);
           reject(error);
         }
       })
@@ -110,11 +119,34 @@ async function transcribeAudio(audioFilePath) {
   });
 }
 
+app.get('/get-transcript', (req, res) => {
+  if (process.env.VUE_APP_LOCAL) {
+    console.log('Local mode, grabbing local JSON file');
+    const jsonFilePath = path.join(__dirname, 'downloads/asrOutput.json')
+    fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Error reading JSON file: ${err.message}`);
+        return res.status(500).json({ error: 'Failed to read JSON file' });
+      }
+
+      try {
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+      } catch (parseError) {
+        console.error(`Error parsing JSON file: ${parseError.message}`);
+        res.status(500).json({ error: 'Failed to parse JSON file' });
+      }
+    });
+  } else {
+    res.status(200).json({ error: 'Local mode not enabled, functionality not yet implemented' });
+    }
+});
+
 async function uploadToS3(filePath, s3Key) {
   const fileStream = fs.createReadStream(filePath);
 
   const params = {
-    Bucket: 'audio-transcription-node-20',
+    Bucket: s3Bucket,
     Key: s3Key,
     Body: fileStream
   };
