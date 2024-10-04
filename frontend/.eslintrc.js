4883649e0c986fa313858ac3e699c135125488d2
ryan terronez <ryanterronez@gmail.com>
2024-10-04 11:00:30 -0500

    moved styling to css file shared styling between components

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
   ],
   env: {
-    "vue/setup-compiler-macros": true
+    'vue/setup-compiler-macros': true,
   },
   rules: {
-    "no-unused-vars": "error", // Ensure this rule is set to 'error'
-    "prettier/prettier": [
-      "error",
+    'no-unused-vars': 'error', // Ensure this rule is set to 'error'
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
         semi: false,
