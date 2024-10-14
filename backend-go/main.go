7cf20d192b513a48786f1d06d5cc4b6e174be857
ryan terronez <ryanterronez@gmail.com>
2024-10-14 11:35:29 -0500

    adding go backend will switch from node when complete

---
+++ backend-go/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"./initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
